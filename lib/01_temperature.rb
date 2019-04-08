17b30f5e9ac508b5edd160d0bb746653607c4f75
Se7endz <oussama.bendifallahd@gmail.com>
2019-04-08 19:39:45 +0200

    essai

---
+++ lib/01_temperature.rb
@@ -0,0 +1,10 @@
+def temperature_conversion_functions
+	temp = gets.chomp.to_f
+end
+def ftoc(temp)
+	f = (temp - 32) * 5/9
+	end
+def ctof(temp)
+	c = (temp * 9/5 + 32)
+end
+
---
+++ lib/02_calculator.rb
@@ -0,0 +1,27 @@
+nbr1 = gets.chomp.to_f
+nbr2 = gets.chomp.to_f
+nbr3 = gets.chomp.to_f
+ar1 = []
+#ar2 = []
+def add(nbr1,nbr2)
+	ad = (nbr1 + nbr2)
+end
+def subtract(nbr1,nbr2)
+	subtract = (nbr1 - nbr2)
+end
+def sum(ar1)
+	ar1.sum
+end
+def multiply(nbr1,nbr2)
+	multiply = (nbr1 * nbr2)
+end
+def power(nbr1,nbr2)
+	power = (nbr1 ** nbr2)
+end
+def factorial(nbr3)
+	while nbr3 >= 0
+	factorial = nbr3 * (nbr3 - 1)
+	nbr3 += 1
+end
+end
+end
